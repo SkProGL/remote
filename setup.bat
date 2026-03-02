cf8977983ff36e7c0a209e4404773d5cfefd820b
SkProGL <gl.is.watching@gmail.com>
2026-03-02 18:03:20 +0000

    removed duplicate uv env warning

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
-
-uv venv --python 3.11
+uv venv --python 3.11 --clear
 .\.venv\Scripts\activate
 uv pip install -r requirements.txt
 python main.py
