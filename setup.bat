b76908e86718bdb9bbee4f851d25ca0b45c15cc9
SkProGL <gl.is.watching@gmail.com>
2026-03-02 17:42:26 +0000

    added uv env

--- setup.bat
+++ setup.bat
@@ -1,1 +1,6 @@
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+
+uv venv --python 3.11
+.\.venv\Scripts\activate
+uv pip install -r requirements.txt
+python main.py
