96ff5e1145734ac59553a91ebe164fa12b8a40fc
SkProGL <gl.is.watching@gmail.com>
2026-03-16 20:58:02 +0000

    minor changes

--- setup.ps1
+++ setup.ps1
@@ -6,4 +6,5 @@ uv pip install -r requirements.txt
 python main.py
 $env:CUSTOM_MSVC = [Environment]::GetEnvironmentVariable("CUSTOM_MSVC","User")
 $env:CUSTOM_WINSDK = [Environment]::GetEnvironmentVariable("CUSTOM_WINSDK","User")
-cp rclone/rclone.conf C:\Users\g2-leonovs\AppData\Roaming\rclone\rclone.conf
+mkdir C:\Users\g2-leonovs\AppData\Roaming\rclone
+cp rclone.conf C:\Users\g2-leonovs\AppData\Roaming\rclone\rclone.conf
